ff96ce95bcf05e06b2ea3b484b85069c809d3b23
Heather Riddle and Mike Wellen <pair+heather+mike@holyguacamole.io>
2018-02-08 09:08:42 -0600

    logging for bad token

--- src/main/kotlin/io/holyguacamole/bot/controller/EventController.kt
+++ src/main/kotlin/io/holyguacamole/bot/controller/EventController.kt
@@ -5,19 +5,24 @@ import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeName
 import io.holyguacamole.bot.message.MessageService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Logger
 
 @RestController
 class EventController(@Value("\${slack.token}") val token: String, val service: MessageService) {
 
+    private val log = LoggerFactory.getLogger(EventController::class.java)
+
     @PostMapping("/messages")
     fun message(@RequestBody request: SlackRequest): ResponseEntity<SlackResponse> =
             if (request.token != token) {
+                log.error("Attempted with token: ${request.token}")
                 ResponseEntity.status(401).build()
             } else {
                 when (request) {
