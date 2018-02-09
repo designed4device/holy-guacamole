9fd50c07730f08e2bbe5fd1dcf228301f567f445
Heather Riddle and Mike Wellen <pair+heather+mike@holyguacamole.io>
2018-02-09 15:57:18 -0600

    springboottest instead of datamongotest to fix this ish

--- src/test/kotlin/io/holyguacamole/bot/message/RepositoryTest.kt
+++ src/test/kotlin/io/holyguacamole/bot/message/RepositoryTest.kt
@@ -1,25 +1,37 @@
 package io.holyguacamole.bot.message
 
-//@RunWith(SpringRunner::class)
-//@DataMongoTest
-//class RepositoryTest {
-//
-//    @Autowired lateinit var repository: AvocadoReceiptRepository
-//
-//    @Before
-//    fun setUp() {
-//        repository.saveAll(MockAvocadoReceipts.singleMentionAndSingleAvocadoReceipts)
-//    }
-//
-//    @After
-//    fun tearDown() {
-//        repository.deleteAll()
-//    }
-//
-//    @Test
-//    fun `it finds AvocadoReceipts by eventId`() {
-//        val avocadoReceipt = MockAvocadoReceipts.singleMentionAndSingleAvocadoReceipts.first()
-//
-//        assert(repository.findByEventId(avocadoReceipt.eventId)).containsExactly(avocadoReceipt)
-//    }
-//}
+import assertk.assert
+import assertk.assertions.containsExactly
+import io.holyguacamole.bot.MockAvocadoReceipts
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RepositoryTest {
+
+    @Autowired lateinit var repository: AvocadoReceiptRepository
+
+    @Before
+    fun setUp() {
+        repository.saveAll(MockAvocadoReceipts.singleMentionAndSingleAvocadoReceipts)
+    }
+
+    @After
+    fun tearDown() {
+        repository.deleteAll()
+    }
+
+    @Test
+    fun `it finds AvocadoReceipts by eventId`() {
+        val avocadoReceipt = MockAvocadoReceipts.singleMentionAndSingleAvocadoReceipts.first()
+
+        assert(repository.findByEventId(avocadoReceipt.eventId)).containsExactly(avocadoReceipt)
+    }
+}
