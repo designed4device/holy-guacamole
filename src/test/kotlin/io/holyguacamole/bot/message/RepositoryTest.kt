0ebf94b1378ae5364d1e4701051ad1ffc9de6d3a
Heather Riddle and Mike Wellen <pair+heather+mike@holyguacamole.io>
2018-02-12 08:38:55 -0600

    this is super frustrating

--- src/test/kotlin/io/holyguacamole/bot/message/RepositoryTest.kt
+++ src/test/kotlin/io/holyguacamole/bot/message/RepositoryTest.kt
@@ -8,7 +8,6 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
