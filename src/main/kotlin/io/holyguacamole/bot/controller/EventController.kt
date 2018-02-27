0cf0a25c57b058e3cab4d19f26f0f4ff09db9f50
Heather Riddle and Mike Wellen <pair+heather+mike@holyguacamole.io>
2018-02-27 14:53:47 -0600

    attempting to log the request payload

--- src/main/kotlin/io/holyguacamole/bot/controller/EventController.kt
+++ src/main/kotlin/io/holyguacamole/bot/controller/EventController.kt
@@ -3,11 +3,14 @@ package io.holyguacamole.bot.controller
 import io.holyguacamole.bot.message.EventService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.filter.CommonsRequestLoggingFilter
 
 @RestController
 class EventController(@Value("\${slack.token.verification}") val token: String, val service: EventService) {
@@ -23,7 +26,7 @@ class EventController(@Value("\${slack.token.verification}") val token: String, 
                 log.info(request.toString())
                 when (request) {
                     is UrlVerification -> ResponseEntity.ok(UrlVerificationResponse(challenge = request.challenge) as SlackResponse)
-                    is EventCallback ->  {
+                    is EventCallback -> {
                         service.process(request)
                         ResponseEntity.status(200).build()
                     }
@@ -31,4 +34,18 @@ class EventController(@Value("\${slack.token.verification}") val token: String, 
                 }
             }
 
+}
+
+
+@Configuration
+class LoggingFilterConfig {
+    @Bean
+    fun logFilter(): CommonsRequestLoggingFilter  =
+            CommonsRequestLoggingFilter().apply {
+                setIncludeQueryString(true)
+                setIncludePayload(true)
+                setMaxPayloadLength(10000)
+                isIncludeHeaders = true
+                setAfterMessagePrefix("REQUEST DATA: ")
+            }
 }
